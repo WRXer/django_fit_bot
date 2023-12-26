1d131471421e89efaa6a08e42bed126b6c47c8d1
WRXer <wrxerrr@gmail.com>
2023-12-26 15:30:29 +0400

    создал пользователю возможность добавлять свою тренировку со свим названием

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -2,48 +2,63 @@ import telebot
 from telebot import types
 from telebot.async_telebot import AsyncTeleBot
 from django.conf import settings
-
 from bot.database_sync import get_or_create_user, create_workout, get_user_workouts
 
-bot = AsyncTeleBot(settings.TOKEN_BOT, parse_mode='HTML')
 
+bot = AsyncTeleBot(settings.TOKEN_BOT, parse_mode='HTML')
 telebot.logger.setLevel(settings.LOG_LEVEL)
 
+user_states = {}    #Словарь для хранения состояний пользователей
 
-# Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 async def send_welcome(message):
+    """
+    Стартовый обработчик
+    :param message:
+    :return:
+    """
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button_create_workout = types.KeyboardButton("Создать тренировку")
     button_my_workouts = types.KeyboardButton("Мои тренировки")
     keyboard.add(button_create_workout, button_my_workouts)
-    # Отправляем приветственное сообщение с клавиатурой
-    await bot.send_message(message.chat.id, "Привет! Я бот для тренировок. Выбери действие:", reply_markup=keyboard)
+    await bot.send_message(message.chat.id, "Привет! Я бот для тренировок. Выбери действие:", reply_markup=keyboard)    #Отправляем приветственное сообщение с клавиатурой
 
 
 
-# Обработчик кнопки "Создать тренировку"
 @bot.message_handler(func=lambda message: message.text == "Создать тренировку")
 async def create_workout_handler(message):
-    # Здесь вы можете запросить у пользователя необходимую информацию о тренировке
-    # Например, использовать клавиатуру с вопросами или ждать текстового ввода
+    """
+    Обработчик кнопки "Создать тренировку"
+    Здесь вы можете запросить у пользователя необходимую информацию о тренировке
+    Например, использовать клавиатуру с вопросами или ждать текстового ввода
+    """
+    await bot.send_message(message.chat.id, "Введите название тренировки:")
+    user_states[message.chat.id] = "waiting_for_workout_name"
 
-    # После получения данных о тренировке
-    # Замените этот код на ваш способ получения данных о тренировке
+
+@bot.message_handler(func=lambda message: user_states.get(message.chat.id) == "waiting_for_workout_name")
+async def process_workout_name(message):
+    """
+    Получение данных для создания тренировки
+    :param message:
+    :return:
+    """
+
     user_id = message.from_user.id
     telegram_user = await get_or_create_user(user_id)
 
-    workout_data = {
-        'name': 'Тренировка 1',
+    workout_name = message.text
 
-        # Другие поля модели Workout
+    workout_data = {
+        'name': workout_name,
     }
 
     # Создаем тренировку и записываем в базу данных
     workout = await create_workout(telegram_user, workout_data)
+    del user_states[message.chat.id]
 
     # Отправляем пользователю подтверждение
-    await bot.send_message(message.chat.id, f"Тренировка {workout.name} создана успешно!")
+    await bot.send_message(message.chat.id, f"Тренировка '{workout.name}' создана успешно!")
 
 
 # Обработчик кнопки "Мои тренировки"
